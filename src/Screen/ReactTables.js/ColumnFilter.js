69a729ac62800f46f47a0b4c7f98503ac9894b29
MahtabBukhari <git config --global user.email you@example.com>
2022-10-11 20:30:08 +0500

    apply Debouncing on filters for better searching

--- src/Screen/ReactTables.js/ColumnFilter.js
+++ src/Screen/ReactTables.js/ColumnFilter.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
 
 const ColumnFilter = ({column}) => {
     const {filterValue,setFilter}=column
+
+    const [value,setValue]=useState(filterValue)
+
+    const onChange=useAsyncDebounce(value=>{
+        setFilter(value||undefined)
+    },1000)
+
   return (
     <span>
     Search:{' '}
     <input   
-    value={filterValue || ''}
-    onChange={e=>{setFilter(e.target.value)}}
+    value={value || ''}
+    onChange={e=>{
+        setValue(e.target.value)
+        onChange(e.target.value)
+    }}
     />
  </span>
   )
--- src/Screen/ReactTables.js/GlobalFilter.js
+++ src/Screen/ReactTables.js/GlobalFilter.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { useAsyncDebounce } from 'react-table'
 
 const GlobalFilter = ({gfilter, setGFilter}) => {
+    const [value,setValue]=useState(gfilter)
+    const onChange=useAsyncDebounce(value=>{
+        setGFilter(value || undefined)
+    },1000)
   return (
      <span>
         Search:{' '}
         <input   
-        value={gfilter || ''}
-        onChange={e=>{setGFilter(e.target.value)}}
+        value={value || ''}
+        onChange={e=>{
+            setValue(e.target.value)
+            onChange(e.target.value)
+        }}
         />
      </span>
   )
